7db44f27a397d930927300cc1cc940cc89c1a251
AnaBeloborodova <uroslovanasta@gmail.com>
2021-03-30 20:35:33 +0700

    :fead blog

--- src/ pages/Blog.js
+++ src/ pages/Blog.js
@@ -1,10 +1,81 @@
 import React, {Component} from 'react';
-import CarouselBox from "../components/carouselBox";
+import {Media, Container, Col, Row, Card, ListGroup} from "react-bootstrap";
+import jsImg from '../assets/js.png'
 
 class Blog extends Component {
     render() {
         return (
-            <CarouselBox></CarouselBox>
+            <Container>
+                <Row>
+                    <Col md="9">
+                        <Media className="m-5">
+                            <img width={150} height={150} className="mr-3" src={jsImg} alt="logo"/>
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque aut
+                                    consectetur consequuntur cumque delectus ea eligendi facilis fugit laborum minima,
+                                    nostrum, obcaecati officiis optio porro qui totam unde vero!</p>
+
+
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img width={150} height={150} className="mr-3" src={jsImg} alt="logo"/>
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque aut
+                                    consectetur consequuntur cumque delectus ea eligendi facilis fugit laborum minima,
+                                    nostrum, obcaecati officiis optio porro qui totam unde vero!</p>
+
+
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img width={150} height={150} className="mr-3" src={jsImg} alt="logo"/>
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque aut
+                                    consectetur consequuntur cumque delectus ea eligendi facilis fugit laborum minima,
+                                    nostrum, obcaecati officiis optio porro qui totam unde vero!</p>
+
+
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img width={150} height={150} className="mr-3" src={jsImg} alt="logo"/>
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque aut
+                                    consectetur consequuntur cumque delectus ea eligendi facilis fugit laborum minima,
+                                    nostrum, obcaecati officiis optio porro qui totam unde vero!</p>
+
+
+                            </Media.Body>
+                        </Media>
+
+                    </Col>
+                    <Col md="3">
+                        <h5 className="text-center mt-5">Categories</h5>
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>HTML/CSS</ListGroup.Item>
+                                <ListGroup.Item>JavaScript</ListGroup.Item>
+                                <ListGroup.Item>Python</ListGroup.Item>
+                                <ListGroup.Item>Java</ListGroup.Item>
+                                <ListGroup.Item>C++</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                        <Card className="mt-3" bg="light">
+                            <Card.Body>
+                                <Card.Title>
+                                    Side widget
+                                </Card.Title>
+                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab architecto aspernatur</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
--- src/ pages/Home.js
+++ src/ pages/Home.js
@@ -1,10 +1,53 @@
 import React, {Component} from 'react';
 import CarouselBox from "../components/carouselBox";
+import {Container, CardDeck, Card, Button} from 'react-bootstrap';
 
 class Home extends Component {
     render() {
         return (
-            <CarouselBox></CarouselBox>
+            <>
+                <CarouselBox/>
+                <Container>
+                    <h2 className="text-center m-4">Our team</h2>
+                    <CardDeck className="m-4">
+                        <Card border="success" text="primary">
+                            <Card.Img variant="top" src="https://static.tildacdn.com/tild3366-6139-4636-b832-383166653866/dorabsayt-1.jpeg"/>
+                            <Card.Body>
+                                <Card.Title>Developers</Card.Title>
+                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque
+                                    deserunt, doloremque eaque excepturi exercitationem facilis hic necessitatibus omnis
+                                    placeat quod, rem sapiente soluta temporibus vero. Adipisci minima placeat
+                                    repudiandae?</Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Developers</Card.Title>
+                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque
+                                    deserunt, doloremque eaque excepturi exercitationem facilis hic necessitatibus omnis
+                                    placeat quod, rem sapiente soluta temporibus vero. Adipisci minima placeat
+                                    repudiandae?</Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                            <Card.Img variant="bottom" src="https://leaditteam.com/storage/article-preview/app-article/33/prew_b/how-to-collaborate-efficiently-with-your-outsourced-software-team.jpg?t=1559895923"/>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="https://www.sbpo.ru/loadfiles/moduls/doska/bigfoto/e20dcda5ab81a46819583fe1037ae14b.png"/>
+                            <Card.Body>
+                                <Card.Title>Developers</Card.Title>
+                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque
+                                    deserunt, doloremque eaque excepturi exercitationem facilis hic necessitatibus omnis
+                                    placeat quod, rem sapiente soluta temporibus vero. Adipisci minima placeat
+                                    repudiandae?</Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                        </Card>
+
+                    </CardDeck>
+
+                </Container>
+            </>
         );
     }
 }
